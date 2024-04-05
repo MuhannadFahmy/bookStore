5910a71295ce373178fb9dc37ba193ec551a7484
Muhannad Fahmy <Muhannadmuhammd_97@yahoo.com>
2024-04-04 18:06:05 -0700

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 
-    'mongodb+srv://muhannadmuhammd97:5LdQq7MoDM0WPkgO@bookstoremern.yvxrh8e.mongodb.net/books-collection?retryWrites=true&w=majority';
